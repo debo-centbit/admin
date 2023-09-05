d37f5a878458e0af822d7b991ad94d18a850d512
Iceprincess2 <favour.adjushi@decagon.dev>
2023-09-05 20:08:31 +0100

    updated test suites

--- src/components/Modal.test.tsx
+++ src/components/Modal.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import SpringModal from './Modal'; // Assuming this is the path to your SpringModal component
+import SpringModal from './Modal';
 
 describe('SpringModal Component', () => {
   test('renders without errors', () => {
@@ -10,28 +10,22 @@ describe('SpringModal Component', () => {
 
   test('opens and closes modal on button click', () => {
     render(<SpringModal />);
-  
-    // Modal should be initially closed
     expect(screen.queryByText('Organization Form')).not.toBeInTheDocument();
   
-    // Open modal
     userEvent.click(screen.getByTestId('buttonClick'));
   
-    // Modal should be open
     const organizationFormText = screen.getByText(/Organization Form/i);
     expect(organizationFormText).toBeInTheDocument();
   
-    // Close modal
     userEvent.click(screen.getByLabelText('close'));
   
-    // Modal should be closed again
+    
     expect(organizationFormText).not.toBeInTheDocument();
   });
   
   test('submits the form on button click', async () => {
     render(<SpringModal />);
     
-    // Open modal
     userEvent.click(screen.getByTestId('buttonClick'));
     
   });
--- src/components/NavBar.test.tsx
+++ src/components/NavBar.test.tsx
@@ -10,7 +10,6 @@ test('renders NavBar correctly', () => {
     </ProSidebarProvider>
   );
 
-  // Check if the NavBar is rendered
   const navBarElement = screen.getByLabelText('account of current user');
   expect(navBarElement).toBeInTheDocument();
 });
@@ -21,34 +20,26 @@ test('toggles sidebar on button click', () => {
       <NavBar check={true} change={() => {}} children="UserName" />
     </ProSidebarProvider>
   );
-
-  // Check if the button for toggling sidebar is present
   const toggleSidebarButton = screen.getByLabelText('open drawer');
   expect(toggleSidebarButton).toBeInTheDocument();
 
-  // Simulate a button click
   fireEvent.click(toggleSidebarButton);
 
-  // You can add assertions here to check the behavior of sidebar toggling
 });
 
 test('changes the switch on input change', () => {
-  const mockChange = jest.fn(); // Mock change handler function
+  const mockChange = jest.fn(); 
   render(
     <ProSidebarProvider>
       <NavBar check={true} change={mockChange} children="UserName" />
     </ProSidebarProvider>
   );
 
-  // Check if the switch input is present
   const switchInput = screen.getByLabelText('Size switch demo');
   expect(switchInput).toBeInTheDocument();
 
-  // Simulate input change
   fireEvent.click(switchInput);
 
-  // Check if the change handler was called
   expect(mockChange).toHaveBeenCalledTimes(1);
 });
 
-// You can add more tests for other functionality in the NavBar component
